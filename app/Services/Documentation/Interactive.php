1f71389fdde3c78890e39c5ba41e90ad90ae98cc
Travis Blasingame <stygian.warlock.v2@gmail.com>
2015-01-10 15:58:18 -0600

    Sub sections look to be working now.

--- app/Services/Documentation/Interactive.php
+++ app/Services/Documentation/Interactive.php
@@ -2,8 +2,11 @@
 
 use App\Services\Contracts\Documentation as DocumentationInterface;
 use App\Services\Documentation;
+use DOMDocument;
+use DOMXPath;
 use Github\Client;
 use Illuminate\Support\Collection;
+use League\CommonMark\CommonMarkConverter;
 
 class Interactive implements DocumentationInterface {
 
@@ -53,21 +56,79 @@ class Interactive implements DocumentationInterface {
     {
         $headers = new Collection($this->github->api('repo')->contents()->show('laravel', 'docs', null, $this->documentation->version));
         $headers = $headers->map(function ($header) {
+            if (strtolower($header['name']) == 'readme.md') {
+                return false;
+            }
+
             return substr($header['name'], 0, -3);
+        })->filter(function ($header) {
+            return $header === false ? false : true;
         });
 
         return $this->prettyArray('sections', $headers);
+    }
+
+    private function getSubs()
+    {
+        $header = new Collection($this->github->api('repo')->contents()->show('laravel', 'docs', $this->documentation->header . '.md', $this->documentation->version));
+        $header = base64_decode($header['content']);
+
+        $html = $this->convertMarkdownToHtml($header);
+
+        $subs = $this->convertHtmlToArray($html);
+
+        return $this->prettyArray('sub sections', $subs);
     }
 
     private function prettyArray($type, $array)
     {
         $array = $array->chunk(8);
 
-        return '*Available document '. $type ." are*:\n" . implode("\n", array_map([$this, 'implode'], $array->toArray()));
+        return '*Available document ' . $type . " are*:\n" . implode("\n", array_map([$this, 'implode'], $array->toArray()));
     }
 
     private function implode($array)
     {
         return implode("\t", $array);
+    }
+
+    /**
+     * @param $header
+     *
+     * @return string
+     */
+    private function convertMarkdownToHtml($header)
+    {
+        $markdown = new CommonMarkConverter;
+
+        $html = html_entity_decode(trim($markdown->convertToHtml($header)));
+
+        return $html;
+    }
+
+    /**
+     * @param $html
+     *
+     * @return array
+     */
+    private function convertHtmlToArray($html)
+    {
+        $subs = new Collection;
+        $dom  = new DOMDocument();
+
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($html);
+
+        $finder    = new DomXPath($dom);
+        $classname = "task-list";
+        $nodes     = $finder->query("//ul");
+
+        foreach ($nodes->item(0)->childNodes as $childNode) {
+            if (count($childNode->childNodes) > 0) {
+                $subs->push(\Str::slug(str_replace('/', 'or', $childNode->childNodes->item(1)->childNodes->item(0)->textContent)));
+            }
+        }
+
+        return $subs;
     }
 }
